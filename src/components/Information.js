c961dd01978ec80689af4dd01b8b3006559c4869
Hittloson <hittlo86@gmail.com>
2023-08-12 03:05:31 +0000

    Added sign up Form

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Registration from './Registration'
+import Contact from './Contact'
 
 function Information() {
   return (
     <div>
-        <Registration/>
+      <Registration/>
+      
     </div>
   )
 }
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -8,38 +8,33 @@ function Registration() {
   return (
     <div className='form-container'>
         <div className='formInput'>
-            <div className='form-header gap-2 '>
-                <h2 className='form-title '>Contact</h2>
-                <p className='sm:text-sm pt-1 md:text-lg'>Already have an account? <a href='#'>Login</a></p>
+            <div className='register-form-header gap-2 '>
+                <p className='sm:text-sm pt-1 md:text-lg '>Already have an account? <a href='#'>Login</a></p>
             </div>
             <div className='form-details flex flex-col'>
-                <label>Full name:</label>
-                <input placeholder='Enter your full name' className='inputs'/>
+                <label>Username:</label>
+                <input placeholder='Enter your username' className='inputs'/>
+            </div>
+            <div className='form-details flex flex-col'>
+                <label>Email:</label>
+                <input placeholder='Enter your email' className='inputs'/>
             </div>
             <div className='form-details flex flex-col'>
                 <label>Phone number:</label>
                 <input placeholder='Enter your phone number' className='inputs'/>
             </div>
             <div className='form-details flex flex-col'>
-                <label>Address:</label>
-                <input placeholder='Enter your address' className='inputs'/>
-            </div>
-            <div className='form-details flex flex-col'>
-                <label>Country:</label>
-                <input placeholder='Enter your country' className='inputs'/>
+                <label>Password:</label>
+                <input placeholder='Enter your password' className='inputs'/>
             </div>
             <div className='save-details'>
                 <input type='checkbox' className='check-input'/>
-                <label>Save this information for next time</label>
+                <label>Agree to Terms and  Condition</label>
                 
             </div>
         </div>
-        <div className='form-links '>
-            <div className='return-to-cart '>
-                <FontAwesomeIcon icon={faAngleLeft} className='return-angle'/>
-                <h2>Return to cart</h2>
-            </div>
-            <button className='next-btn'>Next Step <FontAwesomeIcon icon={faAnglesRight} /></button>
+        <div className='Submit-container '>
+            <button className='submit-btn'>Sign up</button>
         </div>
     </div>
   )
